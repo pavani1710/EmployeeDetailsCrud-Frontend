f6197d2ffadd72d011f69d12d735044b59acc1d4
pavani1710 <aindlapvani@gmail.com>
2024-07-09 12:41:57 +0530

    Frontend_EnployeeDetailsCrud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,33 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch(), // Enable fetch API
+      withInterceptorsFromDi() // Enable interceptors from DI
+    )
   ],
   bootstrap: [AppComponent]
 })
